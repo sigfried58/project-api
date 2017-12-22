946b3ed4bcf0ba3ac85815567ecffe4b79f1efa8
jimmy ibanez <jimmy.ibanez@outlook.com>
2017-12-22 20:59:31 -0300

    Token authentication, by header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  before_action :restrict_access
+  protect_from_forgery with: :exception
+
+  private
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      ApiKey.exists?(access_token: token)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171222231333) do
+ActiveRecord::Schema.define(version: 20171222233010) do
+
+  create_table "api_keys", force: :cascade do |t|
+    t.string "access_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
